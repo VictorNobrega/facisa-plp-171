a098a9c89f64b043d774e89f45eadd2efc1fca5b
Victor Pontes <Victor Pontes>
2017-04-03 16:00:39 -0300

    atividades

---
+++ atividade 3/Hello.pas
@@ -0,0 +1,5 @@
+program Hello;
+begin // inicio do programa em pascal
+  writeln ('Oi, meu nome é Victor'); // mostra a mensagem "Oi, meu nome é Victor" na tela
+  readln // ler um valor, utilizado aqui para o programa ficar em pausa já que ele é mostrado muito rapido. Apertar enter.
+end. // fim do programa
