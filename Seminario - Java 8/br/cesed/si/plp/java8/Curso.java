fd6bb136791f5a3f5232e5449d202fc1a2097542
Victor Pontes <Victor Pontes>
2017-05-04 14:10:34 -0300

    Seminario

---
+++ Seminario - Java 8/br/cesed/si/plp/java8/Curso.java
@@ -0,0 +1,18 @@
+package br.cesed.si.plp.java8;
+public class Curso {
+	private String nome;
+	private int alunos;
+	
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+	
+	public int getAlunos() {
+		return alunos;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+}
